879b2684e0dbb4caeebd27c46e70f76b229ffcbc
Anna <annabaumann@laposte.net>
2020-12-07 23:28:42 +0100

    front / cards / item component + card open on click

---
+++ components/Card.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="card">
+    <div class="card__index">{{ index }}</div>
+    <div class="card__separator"></div>
+    <h4 class="card__title">
+      {{ title }}
+    </h4>
+    <a :href="url" class="card__link" target="_blank">→ écouter</a>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    index: {
+      type: Number,
+      required: false,
+    },
+    title: {
+      type: String,
+      required: false,
+    },
+    url: {
+      type: String,
+      required: false,
+    },
+  },
+};
+</script>
+<style scoped>
+.is-open.accordeon__item:last-of-type + .cards {
+  border-bottom: 2px solid #020202;
+}
+
+.cards {
+  width: 100%;
+  padding: 3vw 0 calc(11vw - 20px);
+  min-height: 75vh;
+  box-sizing: border-box;
+  display: none;
+  flex-wrap: nowrap;
+  overflow: auto;
+  justify-content: space-between;
+  /* padding-bottom: 20px; */
+}
+
+.cards li:last-of-type {
+  margin-right: 0px;
+}
+
+.card {
+  height: 100%;
+  position: relative;
+  width: calc(33.333% - 14px);
+  max-width: calc(33.333% - 14px);
+  min-width: calc(33.333% - 14px);
+  flex-basis: calc(33.333% - 14px);
+  margin-right: 14px;
+}
+
+.card__index {
+  font-size: 3.5vw;
+  margin-bottom: 8px;
+}
+
+.card__separator {
+  width: 100%;
+  height: 2px;
+  background-color: #020202;
+}
+
+.card__title {
+  margin: 16px 0 8vw;
+  font-weight: 400;
+  font-size: 2.5vw;
+  letter-spacing: 1px;
+  width: 95%;
+}
+
+.card__link {
+  font-size: 2.2vw;
+  color: #020202;
+  position: absolute;
+  bottom: 0;
+}
+
+.card__link:hover {
+  font-style: italic;
+}
+</style>
---
+++ components/Item.vue
@@ -0,0 +1,106 @@
+<template>
+  <div>
+    <li :class="`accordeon__item js-toggle-accordeon ${show ? 'is-open' : ''} `" @click="showList">
+      <h3>{{ $prismic.asText(theme.data.title) }}</h3>
+      <img src="~/assets/img/cross.svg" alt="toggle accordeon" class="accordeon__icon" />
+    </li>
+    <!-- créer un component allcard et mettre un LazyAllCards -->
+    <ul class="cards" v-if="show">
+      <Card
+        index="0"
+        title="Détruire l'Université d'André Gorz, suivi d'un commentaire/ café du commerce"
+        url="https://google.com"
+      />
+      <Card
+        index="1"
+        title="Détruire l'Université d'André Gorz, suivi d'un commentaire/ café du commerce"
+        url="https://google.com"
+      />
+      <Card
+        index="2"
+        title="Détruire l'Université d'André Gorz, suivi d'un commentaire/ café du commerce"
+        url="https://google.com"
+      />
+      <Card
+        index="3"
+        title="Détruire l'Université d'André Gorz, suivi d'un commentaire/ café du commerce"
+        url="https://google.com"
+      />
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    theme: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      show: false,
+    };
+  },
+  methods: {
+    showList() {
+      this.show = !this.show;
+    },
+  },
+};
+</script>
+<style scoped>
+.accordeon__item {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  cursor: pointer;
+  margin-bottom: 0.6vw;
+  list-style: none;
+  font-size: 8vw;
+  border-top: 2px solid #020202;
+  padding: 0;
+}
+
+.is-open .accordeon__icon {
+  transform: rotate(45deg);
+}
+
+.cards {
+  width: 100%;
+  padding: 3vw 0 calc(11vw - 20px);
+  min-height: 75vh;
+  display: flex;
+  box-sizing: border-box;
+  flex-wrap: nowrap;
+  overflow-y: scroll;
+  justify-content: space-between;
+}
+
+.accordeon__item:hover {
+  font-style: italic;
+}
+
+.accordeon__icon {
+  margin: 0.6vw 14px;
+  width: 6vw;
+  height: auto;
+}
+
+.accordeon__item:last-of-type {
+  border-bottom: 2px solid #020202;
+}
+
+.accordeon__item.is-open {
+  border-bottom: none;
+}
+
+.is-open.accordeon__item:last-of-type + .cards {
+  border-bottom: 2px solid #020202;
+}
+
+.accordeon__item h3 {
+  margin: 0;
+  font-size: 7vw;
+}
+</style>
